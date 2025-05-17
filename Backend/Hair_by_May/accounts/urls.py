d003896facedf83a9f7da69cd803b3c7bc3026db
Abdulbasit <abdulbasitkayode@gmail.com>
2025-05-17 21:39:34 +0100

    feat: add URL patterns for user authentication and email verification

---
+++ Backend/Hair_by_May/accounts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from .views import RegisterView, LoginView, ProfileView, RefreshTokenView,LogoutView,VerifyEmailView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('token/refresh/', RefreshTokenView.as_view(), name='token_refresh'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('verify-email/<uidb64>/<token>/', VerifyEmailView.as_view(), name='verify_email'),
+    
+]
+
